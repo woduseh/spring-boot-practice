e16b2c3ba7969f22e63caa01703438a690a60727
Hwang Jae Yeon <woduseh@humuson.com>
2022-03-21 14:04:50 +0900

    Style: 텍스트 내용 수정

--- src/test/java/com/springboot/web/practice/domain/index/api/IndexApiTest.java
+++ src/test/java/com/springboot/web/practice/domain/index/api/IndexApiTest.java
@@ -3,6 +3,7 @@ package com.springboot.web.practice.domain.index.api;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,17 +13,18 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-public class IndexApiTest {
+class IndexApiTest {
 
   @Autowired
   private TestRestTemplate restTemplate;
 
   @Test
-  public void 메인페이지_로딩() {
+  @DisplayName("메인 페이지 로딩")
+  void loadIndex() {
     //when
     String body = this.restTemplate.getForObject("/", String.class);
 
     //then
-    assertThat(body).contains("안녕");
+    assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
   }
 }
