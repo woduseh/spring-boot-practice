8a52e0d77f0562f93f92dec8d784068ee9ee9455
Hwang JaeYeon <woduseh@humuson.com>
2022-03-26 07:43:04 +0900

    Fix: deploy.sh 버그 수정
    - chmod 삭제 (실행 권한 부여할 필요 없음)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -22,10 +22,6 @@ JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar|tail -n 1)
 
 echo "> JAR NAME: $JAR_NAME"
 
-echo "> $JAR_NAME 에 실행권한 추가"
-
-chmod +x $JAR_NAME
-
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
