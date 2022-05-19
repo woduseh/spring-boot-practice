cf10364a584b87fcb18b95c3755889d96109fae5
HWANG JAEYEON <woduseh@gmail.com>
2022-05-19 14:52:03 +0900

    Refactor: 코드 간략화

--- src/main/java/com/springboot/web/practice/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/springboot/web/practice/config/auth/dto/OAuthAttributes.java
@@ -27,7 +27,7 @@ public class OAuthAttributes {
   public static OAuthAttributes of(String registrationId, String userNameAttributeName,
       Map<String, Object> attributes) {
     if ("naver".equals(registrationId)) {
-      return ofNaver("id", attributes);
+      return ofNaver(attributes);
     }
 
     return ofGoogle(userNameAttributeName, attributes);
@@ -44,8 +44,7 @@ public class OAuthAttributes {
         .build();
   }
 
-  private static OAuthAttributes ofNaver(String userNameAttributeName,
-      Map<String, Object> attributes) {
+  private static OAuthAttributes ofNaver(Map<String, Object> attributes) {
     Map<String, Object> response = (Map<String, Object>) attributes.get("response");
 
     return OAuthAttributes.builder()
@@ -53,7 +52,7 @@ public class OAuthAttributes {
         .email((String) response.get("email"))
         .picture((String) response.get("profile_image"))
         .attributes(response)
-        .nameAttributeKey(userNameAttributeName)
+        .nameAttributeKey("id")
         .build();
   }
 
